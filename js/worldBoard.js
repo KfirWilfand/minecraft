715833b1fe6b44e6754cfbfa59740eb055f4fe8f
Javier Chames <haimch@gmail.com>
2019-06-20 15:31:15 +0300

    build frst world & fixed trunck file name

--- js/worldBoard.js
+++ js/worldBoard.js
@@ -1,17 +1,28 @@
 $(document).ready(function() {
   let tailsMatrix = [
-    [new Sky(), new Sky(),new Sky(), new Sky(),new Sky(), new Sky(),new Sky(), new Sky()],
-    [new Sky(), new Sky(),new Sky(), new Sky(),new Sky(), new Sky(),new Sky(), new Sky()],
-    [new Sky(), new Sky(),new Sky(), new Sky(),new Sky(), new Sky(),new Sky(), new Sky()],
-    [new Sky(), new Sky(),new Sky(), new Sky(),new Sky(), new Sky(),new Sky(), new Sky()],
-    [new Sky(), new Sky(),new Sky(), new Sky(),new Sky(), new Sky(),new Sky(), new Sky()],
-    [new Sky(), new Sky(),new Sky(), new Sky(),new Sky(), new Sky(),new Sky(), new Sky()],
-    [new Grass(), new Grass(),new Grass(), new Grass(),new Grass(), new Grass(),new Grass(), new Grass()],
-    [new Sky(), new Sky(),new Sky(), new Sky(),new Sky(), new Sky(),new Sky(), new Sky()],
-    [new Sky(), new Sky(),new Sky(), new Sky(),new Sky(), new Sky(),new Sky(), new Sky()],
-    [new Grass(), new Sky(),new Sky(), new Sky(),new Sky(), new Sky(),new Sky(), new Sky()],
-    [new Sky(), new Sky(),new Sky(), new Sky(),new Sky(), new Sky(),new Sky(), new Sky()],
-    [new Grass(), new Grass(),new Grass(), new Grass(),new Grass(), new Grass(),new Grass(), new Grass()]
+    [new Sky(), new Sky(),new Sky(), new Sky(),new Sky(), new Sky(),new Sky(), new Sky(), new Sky(), new Sky(), new Sky(), new Sky(), new Sky(), new Sky(), new Sky(), new Sky(),new Sky(), new Sky(), new Sky(), new Sky()],
+    [new Sky(), new Sky(),new Sky(), new Sky(),new Sky(), new Sky(),new Sky(), new Sky(), new Sky(), new Sky(), new Sky(), new Sky(), new Sky(), new Sky(), new Sky(), new Sky(),new Sky(), new Sky(), new Sky(), new Sky()],
+    [new Sky(), new Sky(),new Sky(), new Sky(),new Sky(), new Sky(),new Sky(), new Sky(), new Sky(), new Sky(), new Sky(), new Sky(), new Sky(), new Sky(), new Sky(), new Sky(),new Sky(), new Sky(), new Sky(), new Sky()],
+    [new Sky(), new Sky(),new Sky(), new Sky(),new Sky(), new Sky(),new Sky(), new Sky(), new Sky(), new Sky(), new Sky(), new Sky(), new Sky(), new Sky(), new Sky(), new Sky(),new Sky(), new Sky(), new Sky(), new Sky()],
+    [new Sky(), new Sky(),new Sky(), new Sky(),new Sky(), new Sky(),new Sky(), new Sky(), new Sky(), new Sky(), new Sky(), new Sky(), new Sky(), new Sky(), new Sky(), new Sky(),new Sky(), new Sky(), new Sky(), new Sky()],
+    [new Sky(), new Sky(),new Sky(), new Sky(),new Sky(), new Sky(),new Sky(), new Sky(), new Sky(), new Sky(), new Sky(), new Sky(), new Sky(), new Sky(), new Sky(), new Sky(),new Sky(), new Sky(), new Sky(), new Sky()],
+    // [new Grass(), new Grass(),new Grass(), new Grass(),new Grass(), new Grass(),new Grass(), new Grass()],
+    [new Sky(), new Sky(),new Sky(), new Sky(),new Sky(), new Sky(),new Sky(), new Sky(), new Sky(), new Sky(), new Sky(), new Sky(), new Sky(), new Sky(), new Sky(), new Sky(),new Sky(), new Sky(), new Sky(), new Sky()],
+    [new Sky(), new Sky(),new Sky(), new Sky(),new Sky(), new Sky(),new Sky(), new Sky(), new Sky(), new Sky(), new Sky(), new Sky(), new Sky(), new Sky(), new Sky(), new Sky(),new Sky(), new Sky(), new Sky(), new Sky()],
+    // [new Grass(), new Sky(),new Sky(), new Sky(),new Sky(), new Sky(),new Sky(), new Sky()],
+    [new Sky(), new Sky(),new Sky(), new Sky(),new Sky(), new Sky(),new Sky(), new Sky(), new Sky(), new Sky(), new Sky(), new Sky(), new Sky(), new Sky(), new Sky(), new Sky(),new Sky(), new Sky(), new Sky(), new Sky()],
+    // [new Grass(), new Grass(),new Grass(), new Grass(),new Grass(), new Grass(),new Grass(), new Grass()]
+    [new Sky(), new Sky(),new Sky(), new Sky(),new Sky(), new Sky(),new Sky(), new Sky(), new Sky(), new Sky(), new Sky(), new Sky(), new Sky(), new Sky(), new Sky(), new Sky(),new Sky(), new Sky(), new Sky(), new Sky()],
+    [new Sky(), new Sky(),new Sky(), new Sky(),new Sky(), new Sky(),new Sky(), new Sky(), new Sky(), new Sky(), new Sky(), new Sky(), new Sky(), new Sky(), new Sky(), new Sky(),new Sky(), new Sky(), new Sky(), new Sky()],
+    [new Sky(), new Sky(),new Sky(), new Sky(),new Sky(), new Sky(),new Sky(), new Sky(), new Sky(), new Sky(), new Sky(), new Sky(), new Sky(), new Sky(), new Sky(), new Sky(),new Sky(), new Sky(), new Sky(), new Sky()],
+    [new Sky(), new Sky(),new Sky(), new Sky(),new Sky(), new Sky(),new Sky(), new Sky(), new Sky(), new Sky(), new Sky(), new Sky(), new Sky(), new Sky(), new Sky(), new TreeBranch(),new TreeBranch(), new TreeBranch(), new Sky(), new Sky()],
+    [new Sky(), new Sky(),new Sky(), new Sky(),new Sky(), new Sky(),new Sky(), new Sky(), new Sky(), new Sky(), new Sky(), new Sky(), new Sky(), new Sky(), new Sky(), new TreeBranch(),new TreeBranch(), new TreeBranch(), new Sky(), new Sky()],
+    [new Sky(), new Sky(),new Sky(), new Sky(),new Sky(), new Sky(),new Sky(), new Sky(), new Sky(), new Sky(), new Sky(), new Sky(), new Sky(), new Sky(), new Sky(), new Sky(),new TreeTrunk(), new Sky(), new Sky(), new Sky()],
+    [new Sky(), new Sky(),new Sky(), new Sky(),new Sky(), new Sky(),new Sky(), new Sky(), new Sky(), new Sky(), new Sky(), new Sky(), new Sky(), new Sky(), new Sky(), new Sky(),new TreeTrunk(), new Sky(), new Sky(), new Sky()],
+    [new Sky(), new Sky(),new Sky(), new Sky(),new Sky(), new Sky(),new Sky(), new Sky(), new Sky(), new Sky(), new Stone(), new Stone(), new Sky(), new Sky(), new Sky(), new Sky(),new TreeTrunk(), new Sky(), new Stone(), new Sky()],
+    [new Soil(), new Soil(),new Grass(), new Grass(),new Grass(), new Grass(),new Grass(), new Soil(),new Soil(), new Soil(),new Soil(), new Soil(),new Soil(), new Soil(),new Soil(), new Soil(), new Soil(),new Soil(), new Soil(), new Soil()],
+    [new Soil(), new Soil(),new Soil(), new Soil(),new Soil(), new Soil(),new Soil(), new Soil(),new Soil(), new Soil(),new Soil(), new Soil(),new Soil(), new Soil(),new Soil(), new Soil(), new Soil(),new Soil(), new Soil(), new Soil()],
+    [new Soil(), new Soil(),new Soil(), new Soil(),new Soil(), new Soil(),new Soil(), new Soil(),new Soil(), new Soil(),new Soil(), new Soil(),new Soil(), new Soil(),new Soil(), new Soil(), new Soil(),new Soil(), new Soil(), new Soil()],
   ];
 
   let tailSize = 30;
