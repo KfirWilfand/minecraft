123e7358132a72a232acb7e8fab445d315ea564f
kfir <kfir333@gmail.com>
2019-06-23 07:32:27 +0300

    add tile functionality

--- js/tile.js
+++ js/tile.js
@@ -1,3 +1,9 @@
+let inventoryPlacementType;
+let pickedInventoryItem;
+let isInventoryOnPlacement = false;
+let tile;
+let isTilePermitToPlacement = false;
+
 function addInventory(type) {
   let inventoryCount = parseInt($(`#${type}-inventory`).text());
   inventoryCount++;
@@ -17,81 +23,106 @@ function getTileTypeByFullClass(classAttr) {
   return type;
 }
 
-let isInventoryOnPlacement = false;
 $(document).ready(function() {
   $(".tile.b-cell")
     .mouseover(function(e) {
+      let row = parseInt(e.target.getAttribute("row"));
+      let col = parseInt(e.target.getAttribute("col"));
       $(this).css("border", "1px dashed black");
       pickedTool.checkDependency(e.target)
         ? pickedTool.setToolPermitted()
         : pickedTool.setToolUnPermitted();
+
+      if (!isInventoryOnPlacement) return;
+      if (
+        tile.checkDependency(tailsMatrix[row + 1][col].type) &&
+        tailsMatrix[row][col].type == "sky"
+      ) {
+        $(e.target).css("border", "1px dashed green");
+        isTilePermitToPlacement = true;
+      } else {
+        $(e.target).css("border", "1px dashed red");
+        isTilePermitToPlacement = false;
+      }
     })
     .mouseout(function(e) {
       $(this).css("border", "none");
       pickedTool.setToolRegular();
     })
     .click(function(e) {
-      let type = getTileTypeByFullClass(e.target.getAttribute("class"));
       let row = parseInt(e.target.getAttribute("row"));
       let col = parseInt(e.target.getAttribute("col"));
 
-      if (!pickedTool.checkDependency(e.target)) return;
+      if (isInventoryOnPlacement) {
+        if (!isTilePermitToPlacement) return;
+        $(e.target).attr("class", `${inventoryPlacementType} tile b-cell`);
 
-      addInventory(type);
-      e.target.setAttribute("class", "sky tile");
+        tailsMatrix[row][col] = tile;
+        return;
+      } else {
+        let type = getTileTypeByFullClass(e.target.getAttribute("class"));
 
-      tailsMatrix[row][col] = new Sky();
+        if (!pickedTool.checkDependency(e.target)) return;
+
+        addInventory(type);
+        e.target.setAttribute("class", "sky tile");
+
+        tailsMatrix[row][col] = new Sky();
+      }
     });
 
   $(".tile.box-inventory").click(function(e) {
-    let type = getTileTypeByFullClass(e.target.getAttribute("class"));
-    let inventoryCount = parseInt($(`#${type}-inventory`).text());
+    isInventoryOnPlacement = true;
+    inventoryPlacementType = getTileTypeByFullClass(
+      e.target.getAttribute("class")
+    );
+
+    let inventoryCount = parseInt(
+      $(`#${inventoryPlacementType}-inventory`).text()
+    );
     if (inventoryCount <= 0) return;
 
-    isInventoryOnPlacement = true;
-    let pickedInventoryItem = $(this);
+    pickedInventoryItem = $(this);
     $(pickedInventoryItem).addClass("box-inventory-picked");
+    tile;
 
-    switch (type) {
-        case "soil":
-          pickedTool = new Soil();
-          break;
-        case "grass":
-          pickedTool = new Grass();
-          break;
-        case "stone":
-          pickedTool = new Stone();
-          break;
-        case "treeTrunk":
-          pickedTool = new TreeTrunk();
-          break;
-        case "treeBranch":
-          pickedTool = new TreeBranch();
-          break;
-      }
+    switch (inventoryPlacementType) {
+      case "soil":
+        tile = new Soil();
+        break;
+      case "grass":
+        tile = new Grass();
+        break;
+      case "stone":
+        tile = new Stone();
+        break;
+      case "treeTrunk":
+        tile = new TreeTrunk();
+        break;
+      case "treeBranch":
+        tile = new TreeBranch();
+        break;
+    }
+  });
 
-    // removeInventory(type);
-
-    $(document)
-      .mousemove(function(e) {
-        if (!isInventoryOnPlacement) return;
-        $(".cursor")
-          .addClass(`${type}`)
-          .show()
-          .css({
-            left: e.clientX,
-            top: e.clientY
-          });
-      })
-      .mousedown(function(e) {
-        let row = parseInt(e.target.getAttribute("row"));
-        let col = parseInt(e.target.getAttribute("col"));
-
-     
-
-        isInventoryOnPlacement = false;
-        $(pickedInventoryItem).removeClass("box-inventory-picked");
-        $(".cursor").hide();
+  $(document).mousemove(function(e) {
+    if (!isInventoryOnPlacement) return;
+    $(".cursor")
+      .addClass(`${inventoryPlacementType}`)
+      .show()
+      .css({
+        left: e.clientX,
+        top: e.clientY
       });
+  });
+
+  $("#main-board").click(function(e) {
+    if (!isInventoryOnPlacement) return;
+    if (!isTilePermitToPlacement) return;
+
+    $(pickedInventoryItem).removeClass("box-inventory-picked");
+    $(".cursor").hide();
+    removeInventory(inventoryPlacementType);
+    isInventoryOnPlacement = false;
   });
 });
